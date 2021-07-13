658d20d7090ead5be3bc7d67bac0555ecd445180
Amy Keibler <amelia.keibler@gmail.com>
2021-07-12 20:32:49 -0400

    Update to newer flake structure from haskell.nix documentation

--- {{ project_name }}/flake.nix
+++ {{ project_name }}/flake.nix
@@ -1,6 +1,5 @@
 {
   description = "{{ project_description }}";
-
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -10,25 +9,25 @@
     let
       overlays = [ haskellNix.overlay
         (final: prev: {
+          # This overlay adds our project to pkgs
           {{ project_name }} =
             final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc8104";
+              compiler-nix-name = "ghc8105";
+              shell.tools = {
+                cabal = {};
+                ghcid = {};
+                haskell-language-server = {};
+                hlint = {};
+              };
             };
         })
       ];
       pkgs = import nixpkgs { inherit system overlays; };
-      flake = pkgs.{{ project_name }}.flake {};
+      flake = pkgs.{{ project_name }}.flake {
+        crossPlatforms = p: [];
+      };
     in flake // {
       defaultPackage = flake.packages."{{ project_name }}:exe:{{ project_name }}";
-
-      devShell = pkgs.{{ project_name }}.shellFor {
-        tools = {
-          cabal = "latest";
-          ghcid = "latest";
-          haskell-language-server = "latest";
-          hlint = "latest";
-        };
-      };
     });
 }
